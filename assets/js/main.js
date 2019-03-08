30b3fb2d33461f4ba0dd01a7e869a25cb9c797c9
oOHiyoriOo <wolf.sf75@googlemail.com>
2019-03-08 08:25:03 +0100

    uploaded Website itself....

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+function wtm() {
+	console.log("This code is from \"https://Nami-mc.site\". if you see this msg the code is usaly just copied from us :)")
+	// if you here to delete the watermark: its not the only watermark :// i hided some more in the code :P have fun by seeking it!
+}
---
+++ mail_handler.php
@@ -0,0 +1,17 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "wolf.sf75@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+							// $last_name = $_POST['last_name'];
+    $subject = "Reply von Ad auf http://Nami-mc.site";
+    $message = $name . "Schrieb:" . $_POST['message'];
+    $headers = "From:" . $from;
+	
+    mail($to,$subject,$message,$headers);
+	// echo "E-Mail wurde Gesendet!" . $first_name . ", Wir werden so früh wie möglich antworten.";
+	// You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    header('Location: tobbe.html');
+	}
+?>
